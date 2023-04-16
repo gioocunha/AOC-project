336ab3bdfe49f0522aaede517395bd92703715ee
João Victor Ferreira <jvictorsbferreira@gmail.com>
2023-04-16 15:50:52 -0300

    Programa 4 upgraded

--- Benchmark/PA_de_Razão_Pi-4.py
+++ Benchmark/PA_de_Razão_Pi-4.py
@@ -1,8 +1,15 @@
 import time
+
+tempos = []
+
 inicio = time.time()
 a1 = 1
 r = 3.14159265359
 n = 1
 for n in range(1,1000000):
     an = a1 + (n - 1) * r
-print(time.time()-inicio)
+
+fim = time.time()
+tempos.append(fim-inicio)
+
+print('Tempos de execução em segundos: ', tempos)
